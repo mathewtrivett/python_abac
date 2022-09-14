d3ac742af033bac95cc0b4cd2495fe14cf18b196
Mathew Trivett <mathew@sh24.org.uk>
2022-09-14 09:37:45 +0100

    Adds mkdocs for docs

--- python_abac/__init__.py
+++ python_abac/__init__.py
@@ -1,5 +1,7 @@
 from .enforcer import Enforcer
+from .decider import Decider
 from .request import Request
 from .response import Response, Decision
 
-__all__ = ["Enforcer", "Request", "Response", "Decision"]
+
+__all__ = ["Enforcer", "Request", "Response", "Decision", "Decider"]
--- tests/test_enforcer.py
+++ tests/test_enforcer.py
@@ -1,6 +1,6 @@
 from unittest.mock import MagicMock, AsyncMock
 import pytest
-from python_abac import Enforcer, Response, Decision
+from python_abac import Enforcer, Decider, Response, Decision
 
 
 @pytest.mark.asyncio
@@ -25,7 +25,8 @@ class TestAuthorisationEnforcer:
         Create a mock decision service. Allow the return value
         to be overridden.
         """
-        pass
+        mock_decision_service = MagicMock()
+        mock_decision_service.is_allowed = AsyncMock().return_value
 
     @pytest.mark.parametrize(
         "decision_service,response,allowed", [(truthy_response), (falsy_response)]
